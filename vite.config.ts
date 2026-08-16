83812762f1862f8502c6c8e327061405ea747c30
Muhammed Sinan K P <muhammedsinankp31@gmail.com>
2026-08-16 18:02:35 +0530

    refactor: comment out build configuration in Vite setup

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
-  },
+  },/*
   build: {
     lib: {
       entry: path.resolve(__dirname, "src/index.ts"),
@@ -27,7 +27,7 @@ export default defineConfig({
         },
       },
     },
-  },
+  },*/
 });
 
 
